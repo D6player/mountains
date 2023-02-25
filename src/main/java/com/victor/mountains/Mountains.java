fdd3bfac902b6e9637929221514a32a2cb26d0fb
D6player <victor.mendez@inscanroca.com>
2023-02-25 16:18:09 +0100

    first commit

---
+++ src/main/java/com/victor/mountains/Mountains.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.victor.mountains;
+
+/**
+ *
+ * @author bard
+ */
+public class Mountains {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
